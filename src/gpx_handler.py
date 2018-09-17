c5eb4ad691429025635b1dec4ae2f9264f14879c
Przemyslaw Rokosz <przemyslaw.rokosz@gmail.com>
2018-09-17 23:18:24 +0200

    added calculation of gated error value

--- src/gpx_handler.py
+++ src/gpx_handler.py
@@ -6,26 +6,31 @@ _GPX_TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
 
 class GpxHandler:
-    def __init__(self, filename, running_avg_window):
+    def __init__(self, filename, running_avg_window, hysteresis):
         self.filename = filename
         self.gpx = etree.parse(self.filename)
         self.gpx_timestamps = self.gpx.xpath('//gpx:trkseg/gpx:trkpt/gpx:time', namespaces=_GPX_NAMESPACE)
         self.running_avg_window = running_avg_window
+        self.hysteresis = hysteresis
 
         self.errors = []
         self.running_avg = []
+        self.gated_errors = []
 
     def get_errors(self):
         if len(self.errors) == 0:
             self.__calculate_errors()
-        print("self.errors = " + str(len(self.errors)))
         return self.errors
 
     def get_running_average(self):
         if len(self.running_avg) == 0:
             self.__calculate_running_average()
-        print("self.running_avg = " + str(len(self.running_avg)))
         return self.running_avg
+
+    def get_gated_errors(self):
+        if len(self.gated_errors) == 0:
+            self.__calculate_gated_errors()
+        return self.gated_errors
 
     def __calculate_errors(self):
         if len(self.errors) > 0:
@@ -58,3 +63,25 @@ class GpxHandler:
                 self.running_avg.append(ravg)
             elif len(running_avg_elements) > self.running_avg_window / 2:
                 self.running_avg.append(None)
+
+    def __calculate_gated_errors(self):
+        if len(self.gated_errors) > 0:
+            return
+        self.__calculate_running_average()
+
+        tmp_gated_error = 0
+        for avg in self.running_avg:
+            if avg is not None:
+                tmp_gated_error = self.__gate(avg, tmp_gated_error)
+                self.gated_errors.append(tmp_gated_error)
+            else:
+                self.gated_errors.append(None)
+
+    def __gate(self, current_value, last_gated_value):
+        tmp = last_gated_value - current_value
+        if tmp > self.hysteresis:
+            return last_gated_value - 1
+        elif tmp < -self.hysteresis:
+            return last_gated_value + 1
+        else:
+            return last_gated_value
--- src/main.py
+++ src/main.py
@@ -4,17 +4,33 @@ from plotter import plot
 
 _VERSION = "PyGpx 0.0.1"
 _DEFAULT_RUNNING_AVG_WINDOW = 60
+_DEFAULT_HYSTERESIS = 0.7
+
+
+def get_value_change_count(lst):
+    last_val = lst[0]
+    count = 0
+    for val in lst:
+        if val != last_val:
+            if last_val is not None:
+                count = count + 1
+            last_val = val
+    return count
+
 
 def main():
     parser = argparse.ArgumentParser(description='PyGpx - tool for GPX file analysis and correction')
     parser.add_argument('--version', action='version', version=_VERSION)
     parser.add_argument('-p', '--plot', dest='plot', action='store_true', help='plot file analysis result')
     parser.add_argument('gpx_file', metavar='FILE', help='path to GPX file')
-    parser.add_argument('--running-avg', dest='running_avg_window', action='store', default=_DEFAULT_RUNNING_AVG_WINDOW)
+    parser.add_argument('--running-avg-window', metavar='W', dest='running_avg_window', action='store', help='running average windows size (default: ' + str(_DEFAULT_RUNNING_AVG_WINDOW) + ')', default=_DEFAULT_RUNNING_AVG_WINDOW)
+    parser.add_argument('--hysteresis', metavar='H', dest='hysteresis', action='store', help='hysteresis value (default: ' + str(_DEFAULT_HYSTERESIS) + ')', default=_DEFAULT_HYSTERESIS)
+
     args = parser.parse_args()
 
-    handler = GpxHandler(args.gpx_file, args.running_avg_window)
-    plot(handler.get_errors(), handler.get_running_average())
+    handler = GpxHandler(args.gpx_file, args.running_avg_window, args.hysteresis)
+    print("Gated errors values change " + str(get_value_change_count(handler.get_gated_errors())) + " times")
+    plot(handler.get_errors(), handler.get_running_average(), handler.get_gated_errors())
 
 
 if __name__ == "__main__":
